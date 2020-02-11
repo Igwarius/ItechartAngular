95f6c102b5a0e26f207b3282eb88e3eaff9db52f
Igwarius <zolotnik1998@mail.ru>
2020-02-11 18:55:42 +0300

    1

--- itechAngul/src/app/app.component.ts
+++ itechAngul/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { User } from "./app.user";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'itechAngul';
+  title = "registation";
+  text1: string;
+  text2: string;
+  users: User[];
+  addUser(text1: string, text2: string): void {
+    if (
+      text1 == null ||
+      text1.trim() == "" ||
+      text2 == null ||
+      text2.trim() == ""
+    )
+      return;
+    this.users.push(new User(text1, text2));
+  }
 }
