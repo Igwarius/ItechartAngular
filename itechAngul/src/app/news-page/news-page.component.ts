25cfa9cbdc42b3deab33c63293df9a5da62baf4d
Igwarius <zolotnik1998@mail.ru>
2020-02-27 17:04:39 +0300

    add category for main page

--- itechAngul/src/app/news-page/news-page.component.ts
+++ itechAngul/src/app/news-page/news-page.component.ts
@@ -2,21 +2,42 @@ import { Component, OnInit } from "@angular/core";
 import { News } from "src/app/Models/news";
 import { HttpClient } from "@angular/common/http";
 import { NgxSpinnerService } from "ngx-spinner";
+import { Catigories } from "../Models/categories";
 @Component({
   selector: "app-news-page",
   templateUrl: "./news-page.component.html",
   styleUrls: ["./news-page.component.css"]
 })
 export class NewsPageComponent implements OnInit {
+  header: string;
+
   newss: News;
+  catigories: Catigories;
   title = "itechAngul";
   constructor(private http: HttpClient, private spinner: NgxSpinnerService) {}
   ngOnInit() {
+    this.header = "Новости";
     this.spinner.show();
     this.http
       .get("https://localhost:44333/api/News/GetAllNews")
       .subscribe(result => {
         this.newss = <News>result;
+        this.spinner.hide();
+      });
+    this.http
+      .get("https://localhost:44333/api/News/GetAllCategories")
+      .subscribe(result => {
+        this.catigories = <Catigories>result;
+      });
+  }
+  onSelect(categori: Catigories): void {
+    console.log(categori.id);
+    this.spinner.show();
+    this.http
+      .get("https://localhost:44333/api/News/GetNewsByCategory/" + categori.id)
+      .subscribe(result => {
+        this.newss = <News>result;
+        this.header = categori.name;
         this.spinner.hide();
       });
   }
